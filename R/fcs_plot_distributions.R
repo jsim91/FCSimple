c647f176454b5d04d2988cf220cb106747f85b73
Joshua Simmons <joshua.d.simmons@vumc.org>
2022-10-27 15:15:55 -0500

    plot_dist

--- R/fcs_plot_distributions.R
+++ R/fcs_plot_distributions.R
@@ -1,5 +1,6 @@
 fcs_plot_distributions <- function(fcs_join_obj,
-                                   separate_by = c("none", "date"))
+                                   separate_by = c("none", "date"),
+                                   plot_element = c("cluster","total"))
 {
   if(separate_by=="date"){
     if(!"run_date" %in% names(fcs_join_obj)){
