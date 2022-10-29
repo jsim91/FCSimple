2db8dc496c0480dfb0108e8e6061f585bb5a0117
Joshua Simmons <joshua.d.simmons@vumc.org>
2022-10-29 16:15:23 -0500

    heatmap update

--- R/fcs_cluster_heatmap.R
+++ R/fcs_cluster_heatmap.R
@@ -11,6 +11,11 @@ fcs_cluster_heatmap <- function(fcs_join_obj, algorithm = "leiden",
   require(ComplexHeatmap)
   require(circlize)
 
+  event_source <- fcs_join_obj[["source"]]
+  cluster_numbers <- as.numeric(fcs_join_obj[[tolower(algorithm)]][["clusters"]])
+  heatmap_data <- fcs_join_obj[["data"]]
+  include_channels <- colnames(fcs_join_obj[["data"]])
+
   scaled.global <- CATALYST:::.scale_exprs(t(heatmap_data[,include_channels]), 1, 0.01)
   global.t <- t(scaled.global)
   backend.matrix <- matrix(data=NA,nrow=length(unique(cluster_numbers)),ncol=ncol(global.t))
@@ -27,25 +32,26 @@ fcs_cluster_heatmap <- function(fcs_join_obj, algorithm = "leiden",
   z <- backend.matrix
   color.map.fun = circlize::colorRamp2(seq(min(z),max(z), l = n <- 100), colorRampPalette(hm_pal)(n))
   ncell <- rep(NA,times=length(unique(cluster_numbers)))
-  names(ncell) <- unique(cluster_numbers)[order(unique(cluster_numbers))]
+  names(ncell) <- unique(cluster_numbers)[order(as.numeric(unique(cluster_numbers)))]
   for(i in 1:length(ncell)){
     ncell[i] <- sum(cluster_numbers==names(ncell)[i])
   }
   pop.freq <- matrix(data=ncell,ncol=1)
   row.names(pop.freq) <- names(ncell)
   size_anno_nums <- round((pop.freq/sum(pop.freq))*100,2)
-  # ranno1 <- rowAnnotation(`Cluster\nSize`=anno_barplot(pop.freq,border=F,width=unit(1.75, "cm"),
-  #                                                      axis_param=list(gp=gpar(fontsize=9)), axis = TRUE),
-  #                         annotation_name_gp=gpar(fontsize=10,fontface="bold"), name = "Cluster\nSize")
   ranno1 <- rowAnnotation(`Cluster\nSize`=anno_barplot(pop.freq,border=F,width=unit(1.75, "cm"),
-                                                       axis_param=list(gp=gpar(fontsize=9), at = 0, labels = ""),
-                                                       axis = TRUE, just = "center", location = unit(0.5, "npc"), show_name = TRUE),
-                          annotation_name_gp=gpar(fontsize=10,fontface="bold"), annotation_name_rot = 0, name = "Cluster\nSize")
+                                                       axis_param=list(gp=gpar(fontsize=9)), axis = TRUE),
+                          annotation_name_gp=gpar(fontsize=10,fontface="bold"), name = "Cluster\nSize")
+  # ranno1 <- rowAnnotation(`Cluster\nSize`=anno_barplot(pop.freq,border=F,width=unit(1.75, "cm"),
+  #                                                      axis_param=list(gp=gpar(fontsize=9), at = 0, labels = ""),
+  #                                                      axis = TRUE, just = "center", location = unit(0.5, "npc"), show_name = TRUE),
+  #                         annotation_name_gp=gpar(fontsize=10,fontface="bold"), annotation_name_rot = 0, name = "Cluster\nSize")
   ranno2 <- rowAnnotation(frequency=anno_text(paste0(size_anno_nums,"%"),
                                               gp=gpar(fontsize=10,fontface="bold")))
+  backend.matrix <- backend.matrix[order(as.numeric(row.names(backend.matrix))),]
   heatmap_output <- Heatmap(backend.matrix,col=color.map.fun,
                             row_names_side="left",
-                            name="median\nscaled\nexpression",
+                            #name="median\nscaled\nexpression",
                             heatmap_legend_param=list(at=c(0,0.2,0.4,0.6,0.8,1),legend_height=unit(3,"cm"),
                                                       grid_width=unit(0.6,"cm"),title_position="topleft",
                                                       labels_gp=gpar(fontsize=11),title_gp=gpar(fontsize=11,fontface="bold")),
