e0df989abd605d08d3029dbf28bc4b503d79a68a
Joshua Simmons <joshua.d.simmons@vumc.org>
2022-10-29 17:42:55 -0500

    heatmap grob render

--- R/fcs_cluster_heatmap.R
+++ R/fcs_cluster_heatmap.R
@@ -62,7 +62,7 @@ fcs_cluster_heatmap <- function(fcs_join_obj, algorithm,
 fcs_plot_heatmap <- function(fcs_join_obj, algorithm, outdir = getwd())
 {
   ggsave(filename = paste0(outdir,"/panel_distributions_by_batch_",strftime(Sys.time(),"%Y-%m-%d_%H%M%S"),".pdf"),
-         plot = fcs_join_obj[[paste0(tolower(algorithm),"_heatmap")]][["heatmap"]],
+         plot = grid::grid.grabExpr(fcs_join_obj[[paste0(tolower(algorithm),"_heatmap")]][["heatmap"]]),
          device = "pdf", width = (ncol(fcs_join_obj[[paste0(tolower(algorithm),"_heatmap")]][["heatmap_tile_data"]])*0.5)+2.5,
          height = (nrow(fcs_join_obj[[paste0(tolower(algorithm),"_heatmap")]][["heatmap_tile_data"]])*0.5)+2.5,
          units = "in", dpi = 900)
