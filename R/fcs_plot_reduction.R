4768c99a2159ca0a55244ba037ec7afb5a1cf4f6
Joshua Simmons <joshua.d.simmons@vumc.org>
2022-10-29 18:21:18 -0500

    reduction plotting

--- R/fcs_plot_reduction.R
+++ R/fcs_plot_reduction.R
@@ -1,4 +1,5 @@
-fcs_plot_reduction <- function()
+fcs_plot_reduction <- function(fcs_join_obj, algorithm, reduction)
 {
-
+  reduction_coords <- fcs_join_obj[[tolower(reduction)]][["coordinates"]]
+  cluster_numbers <- fcs_join_obj[[tolower(algorithm)]][["clusters"]]
 }
