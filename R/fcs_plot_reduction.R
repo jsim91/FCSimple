2e60a9172ba115f579dfdb787778089d7fb06042
Joshua Simmons <joshua.d.simmons@vumc.org>
2022-10-29 19:56:33 -0500

    cluster plot update

--- R/fcs_plot_reduction.R
+++ R/fcs_plot_reduction.R
@@ -12,8 +12,11 @@ fcs_plot_reduction <- function(fcs_join_obj, algorithm, reduction, point_alpha =
     xval[i] <- median(reduction_coords[,1][which(cluster_numbers==as.numeric(names(xval)[i]))])
     yval[i] <- median(reduction_coords[,2][which(cluster_numbers==as.numeric(names(yval)[i]))])
   }
+  plt_input <- cbind(reduction_coords,data.frame(cluster = cluster_numbers))
+  plt_input$cluster <- factor(plt_input$cluster)
   plt_reduction <- ggplot(data = reduction_coords, mapping = aes_string(x = colnames(reduction_coords)[1],
-                                                                        y = colnames(reduction_coords)[2])) +
+                                                                        y = colnames(reduction_coords)[2],
+                                                                        color = )) +
     ggrastr::geom_point_rast(alpha = point_alpha) +
     annotate("shadowtext", x = xval, y = yval, label = names(xval), size = 5) +
     theme_void()
