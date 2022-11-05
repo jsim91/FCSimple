35ba8fb0005d01f9cc2f0b536ff043e448c2d539
Joshua Simmons <joshua.d.simmons@vumc.org>
2022-11-04 23:28:56 -0500

    app update

--- R/app/app.R
+++ R/app/app.R
@@ -22,26 +22,35 @@ ui <- fluidPage(theme = shinytheme("yeti"),
                              uiOutput("transform_type"),
                              uiOutput("hyperparameters"),
                              hr(),
-                             actionButton(inputId = "apply_button", label = "apply transform"),
+                             actionButton(inputId = "apply_transform", label = "apply transform"),
                              hr(),
                              uiOutput("channel_x"),
-                             uiOutput("channel_y")
+                             uiOutput("channel_y"),
+                             actionButton(inputId = "apply_plot", label = "update 2d plot")
                            ),
                            mainPanel(
                              plotOutput("densPlot"),
+                             textOutput("dens_hyperlog_error"),
+                             textOutput("dens_asinh_error"),
                              hr(),
                              plotOutput("biax"),
                              hr(),
                              uiOutput("transform_text")
                            )),
-                  tabPanel("translate")
-                )
+                  tabPanel("transform history",
+                           tableOutput("parameter_df")
+                ))
 )
 
 # Define server logic ----
 server <- function(input, output) {
-  # Data <- read.csv("E:/sample_FCS/test_data.csv", check.names = FALSE)
-  Data <- read.csv("C:/Users/simmonjd/Documents/sample_FCS/test_data.csv", check.names = FALSE)
+  Data <- read.csv("E:/sample_FCS/test_data.csv", check.names = FALSE)
+  param_df <- as.data.frame(matrix(data = NA,nrow=9,ncol=ncol(Data)))
+  colnames(param_df) <- colnames(Data)
+  row.names(param_df) <- c("algo","asinh_cofactor","biexp_pos","biexp_neg","biexp_width",
+                           "hyperlog_T","hyperlog_M","hyperlog_W","hyperlog_A")
+  param_settings <- reactiveValues(reactive_data = NULL)
+  param_settings$reactive_data <- param_df
   output$channel <- renderUI({
     selectInput("channel", h4("channel"), choices = colnames(Data))
   })
@@ -66,11 +75,6 @@ server <- function(input, output) {
       }
     }
   })
-  # output$apply <- renderUI({
-  #   if(!is.null(input$transform_type)) {
-  #     actionButton(inputId = "apply_button", label = "apply")
-  #   }
-  # })
   output$channel_x <- renderUI({
     selectInput("channel_x", h5("x-axis channel"), choices = colnames(Data))
   })
@@ -84,35 +88,137 @@ server <- function(input, output) {
       if(input$transform_type=="linear") {
         plot(density(in_data), main = input$channel, xlab = "intensity", ylab = "density")
       } else if(input$transform_type=="asinh") {
-        get_cofactor <- input$cofactor
-        plot(density(base::asinh(in_data/get_cofactor)), main = input$channel, xlab = "intensity", ylab = "density")
+        if(all(!is.null(input$cofactor),input$cofactor!=0)) {
+          get_cof <- input$cofactor
+          plot(density(base::asinh(in_data/get_cof )), main = input$channel, xlab = "intensity", ylab = "density")
+        }
       } else if(input$transform_type=="biexponential") {
-        biexp_fun <- flowWorkspace::flowjo_biexp(pos = input$biexp_pos,
-                                                 neg = input$biexp_neg,
-                                                 widthBasis = (10^input$biexp_width)*-1)
-        plot(density(biexp_fun(in_data)), main = input$channel, xlab = "intensity", ylab = "density")
+        if(all(!is.null(input$biexp_pos),!is.null(input$biexp_neg),!is.null((10^input$biexp_width)*-1))) {
+          biexp_fun <- flowWorkspace::flowjo_biexp(pos = input$biexp_pos,
+                                                   neg = input$biexp_neg,
+                                                   widthBasis = (10^input$biexp_width)*-1)
+          plot(density(biexp_fun(in_data)), main = input$channel, xlab = "intensity", ylab = "density")
+        }
       } else if(input$transform_type=="hyperlog") {
-        hyperlog_fun <- flowCore::hyperlogtGml2(parameters = "tmp", T = input$hyperlog_T, M = input$hyperlog_M,
-                                                W = input$hyperlog_W, A = input$hyperlog_A)
-        plot(density(eval(hyperlog_fun)(in_hyperlog)), main = input$channel, xlab = "intensity", ylab = "density")
+        if(all(!is.null(input$hyperlog_T),!is.null(input$hyperlog_M),!is.null(input$hyperlog_W),!is.null(input$hyperlog_A))) {
+          # in the future, make apply transform button unclickable if hyperlogtGml2 function is invalid, see error messages when certain values are selected
+          # consider https://stackoverflow.com/questions/40621393/disabling-buttons-in-shiny
+          if(all(input$hyperlog_W<(input$hyperlog_M/2), input$hyperlog_A<=(input$hyperlog_M - (input$hyperlog_W * 2)))) {
+            hyperlog_fun <- flowCore::hyperlogtGml2(parameters = "tmp", T = input$hyperlog_T, M = input$hyperlog_M,
+                                                    W = input$hyperlog_W, A = input$hyperlog_A)
+            plot(density(eval(hyperlog_fun)(in_hyperlog)), main = input$channel, xlab = "intensity", ylab = "density")
+          }
+        }
       }
     }
   })
-  cur_transf <- reactiveValues(chosen_transform = NULL)
-  observeEvent(input$apply_button, {
-    cur_transf$chosen_transform <- input$transform_type
-  })
-  output$transform_text <- renderText({
-    if(is.null(cur_transf$chosen_transform)) {
-      return()
-    } else {
-      return(cur_transf$chosen_transform)
+  output$dens_hyperlog_error <- renderText({
+    if(all(!is.null(input$channel),input$transform_type=="hyperlog")) {
+      if(any(input$hyperlog_W>=(input$hyperlog_M/2), input$hyperlog_A>(input$hyperlog_M - (input$hyperlog_W * 2)))) {
+        paste0("W must be less than M/2 .. AND .. A must be less than or equal to M - 2W")
+      }
     }
   })
-  output$biax <- renderPlot({
-    plot(x = Data[,input$channel_x], y = Data[,input$channel_y], pch = 19, col = scales::alpha("black", 0.2),
-         xlab = input$channel_x, ylab = input$channel_y, cex = 0.7) # inherit transform chosen and parameter choice/alter the Data directly when transform is applied with button, when button is selected, first take Data back to pre-transform then apply transform
+  output$dens_asinh_error <- renderText({
+    if(all(!is.null(input$channel),input$transform_type=="asinh")) {
+      if(all(!is.null(input$cofactor),input$cofactor==0)) {
+        "cofactor must not equal 0"
+      }
+    }
   })
+  # observeEvent(input$apply_transform, {
+  #   chosen_channel <- input$channel
+  #   if(input$transform_type=="linear") {
+  #     param_list <- list(algo = input$transform_type)
+  #   } else if(input$transform_type=="asinh") {
+  #     param_list <- list(
+  #       algo = input$transform_type,
+  #       chosen_cof <- input$cofactor
+  #     )
+  #   } else if(input$transform_type=="biexponential") {
+  #     param_list <- list(
+  #       algo = input$transform_type,
+  #       chosen_p <- input$biexp_pos,
+  #       chosen_n <- input$biexp_neg,
+  #       chosen_w <- input$biexp_width
+  #     )
+  #   } else if(input$transform_type=="hyperlog") {
+  #     param_list <- list(
+  #       algo = input$transform_type,
+  #       chosen_t <- input$hyperlog_T,
+  #       chosen_m <- input$hyperlog_M,
+  #       chosen_w <- input$hyperlog_W,
+  #       chosen_a <- input$hyperlog_A
+  #     )
+  #   }
+  #   hold_parameters[[chosen_channel]] <- param_list
+  #   # textLog(paste0(textLog(),"\n",unlist(hold_parameters[[chosen_channel]])))
+  # })
+
+  chosen_transf <- eventReactive(input$apply_transform, {
+    if(is.null(input$transform_type)) {
+      return("")
+    } else {
+      tmp_transform_choice <- input$transform_type
+      if(input$transform_type=="linear") {
+        return(paste0(input$channel,": ",tmp_transform_choice))
+      } else if(input$transform_type=="asinh") {
+        tmp_cof <- input$cofactor
+        return(paste0(input$channel,": ",tmp_transform_choice," - ",tmp_cof))
+      } else if(input$transform_type=="biexponential") {
+        tmp_pos <- input$biexp_pos
+        tmp_neg <- input$biexp_neg
+        tmp_wid <- (10^input$biexp_width)*-1
+        return(paste0(input$channel,": ",tmp_transform_choice," - ",tmp_pos," - ",tmp_neg," - ",tmp_wid))
+      } else if(input$transform_type=="hyperlog") {
+        tmp_t <- input$hyperlog_T
+        tmp_m <- input$hyperlog_M
+        tmp_w <- input$hyperlog_W
+        tmp_a <- input$hyperlog_A
+        return(paste0(input$channel,": ",tmp_transform_choice," - ",tmp_t," - ",tmp_m," - ",tmp_w," - ",tmp_a))
+      }
+    }
+  })
+  output$transform_text <- renderText({
+    chosen_transf()
+  })
+  insert_values <- eventReactive(input$apply_transform, {
+    if(is.null(input$transform_type)) {
+      return("")
+    } else {
+      tmp_transform_choice <- input$transform_type
+      if(input$transform_type=="linear") {
+        return(paste0(input$channel,",",tmp_transform_choice))
+      } else if(input$transform_type=="asinh") {
+        tmp_cof <- input$cofactor
+        return(paste0(input$channel,",",tmp_transform_choice,",",tmp_cof))
+      } else if(input$transform_type=="biexponential") {
+        tmp_pos <- input$biexp_pos
+        tmp_neg <- input$biexp_neg
+        tmp_wid <- (10^input$biexp_width)*-1
+        return(paste0(input$channel,",",tmp_transform_choice,",",tmp_pos,",",tmp_neg,",",tmp_wid))
+      } else if(input$transform_type=="hyperlog") {
+        tmp_t <- input$hyperlog_T
+        tmp_m <- input$hyperlog_M
+        tmp_w <- input$hyperlog_W
+        tmp_a <- input$hyperlog_A
+        return(paste0(input$channel,",",tmp_transform_choice,",",tmp_t,",",tmp_m,",",tmp_w,",",tmp_a))
+      }
+    }
+  })
+  observeEvent(input$apply_transform, {
+    insert_channel <- gsub(",.*$","",insert_values())
+    col_index <- which(colnames(param_settings$reactive_data)==insert_channel)
+    param_settings$reactive_data[1,col_index] <- insert_values()
+    param_settings$reactive_data <- param_settings$reactive_data
+  })
+  output$biax <- renderPlot({
+    if(!is.null(input$transform_type)) {
+      plot(x = Data[,input$channel_x], y = Data[,input$channel_y], pch = 19, col = scales::alpha("black", 0.2),
+           xlab = input$channel_x, ylab = input$channel_y, cex = 0.7) # inherit transform chosen and parameter choice/alter the Data directly when transform is applied with button, when button is selected, first take Data back to pre-transform then apply transform
+    }
+  })
+  output$parameter_df <- renderTable(t(param_settings$reactive_data), rownames = TRUE)
 }
 
 # Run the app ---- # this will be changed later so that the app is called from fcs_join with runApp
