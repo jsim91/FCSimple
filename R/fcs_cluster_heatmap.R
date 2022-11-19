398a98e51fde25efc02d08f9c5cc82c3e98ed86c
Joshua Simmons <joshua.d.simmons@vumc.org>
2022-11-19 15:28:19 -0600

    remove datetime from dbscane heatmap

--- R/fcs_cluster_heatmap.R
+++ R/fcs_cluster_heatmap.R
@@ -63,7 +63,12 @@ fcs_cluster_heatmap <- function(fcs_join_obj, algorithm,
 fcs_plot_heatmap <- function(fcs_join_obj, algorithm, outdir = getwd())
 {
   require(ggplot2)
-  ggsave(filename = paste0(outdir,"/",tolower(algorithm),"_cluster_heatmap_",strftime(Sys.time(),"%Y-%m-%d_%H%M%S"),".pdf"),
+  if(tolower(algorithm)=="dbscan") {
+    fname <- paste0(outdir,"/",tolower(algorithm),"_cluster_heatmap.pdf")
+  } else {
+    fname <- paste0(outdir,"/",tolower(algorithm),"_cluster_heatmap_",strftime(Sys.time(),"%Y-%m-%d_%H%M%S"),".pdf")
+  }
+  ggsave(filename = fname,
          plot = grid::grid.grabExpr(draw(fcs_join_obj[[paste0(tolower(algorithm),"_heatmap")]][["heatmap"]])),
          device = "pdf", width = (ncol(fcs_join_obj[[paste0(tolower(algorithm),"_heatmap")]][["heatmap_tile_data"]])*0.33)+3.25,
          height = (nrow(fcs_join_obj[[paste0(tolower(algorithm),"_heatmap")]][["heatmap_tile_data"]])*0.3)+2.25,
