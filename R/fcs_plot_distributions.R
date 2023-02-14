fb40db3a5e3a13b1d13d8a18eeeb762664cbbb4b
Joshua Simmons <joshua.d.simmons@vumc.org>
2023-02-14 07:21:35 -0600

    plot distr date

--- R/fcs_plot_distributions.R
+++ R/fcs_plot_distributions.R
@@ -8,6 +8,12 @@ fcs_plot_distribution <- function(fcs_join_obj,
   require(ggplot2)
   require(ggridges)
 
+  fcs_join_obj <- fcs_obj
+  separate_by <- "date"
+  plot_algorithm <- "leiden"
+  outdir <- getwd()
+  plot_palette <- NULL
+
   obj_data <- as.data.frame(fcs_join_obj[["data"]])
   if(separate_by=="date") {
     if(!"run_date" %in% names(fcs_join_obj)){
@@ -23,13 +29,14 @@ fcs_plot_distribution <- function(fcs_join_obj,
     for(i in 1:length(data_split)) {
       data_split[[i]] <- data.frame(val1 = obj_data[,which(colnames(obj_data)==names(data_split)[i])],
                                     batch = obj_data[,"date"])
+      colnames(data_split[[i]])[1] <- colnames(obj_data)[i]
     }
   } else {
     data_split <- vector("list", length = ncol(obj_data))
     names(data_split) <- colnames(obj_data)
     for(i in 1:length(data_split)) {
       data_split[[i]] <- data.frame(val1 = obj_data[,which(colnames(obj_data)==names(data_split)[i])])
-      colnames(data_split[[i]]) <- colnames(obj_data)[i]
+      colnames(data_split[[i]])[1] <- colnames(obj_data)[i]
     }
   }
   if(tolower(separate_by) == "cluster") {
@@ -86,8 +93,9 @@ plot_date <- function(input_data)
 {
   capture_channel <- colnames(input_data)[1]
   colnames(input_data)[1] <- "tmp"
-  plt1 <- ggplot(data = input_data, mapping = aes(x = tmp, group = batch)) +
-    geom_density(lwd = 0.3) +
+  plt1 <- ggplot(data = input_data, mapping = aes(x = tmp, group = batch,
+                                                  color = batch, fill = batch)) +
+    geom_density(size = 0.75, alpha = 0.1) +
     theme_minimal() +
     ggtitle(capture_channel) +
     theme(axis.text.y = element_blank(),
