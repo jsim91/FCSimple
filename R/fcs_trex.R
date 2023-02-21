ac92091a6e7f8c846c8b8beb070dd1b55c10e2b4
Joshua Simmons <joshua.d.simmons@vumc.org>
2023-02-21 14:53:40 -0600

    trex intensity out arg

--- R/fcs_trex.R
+++ R/fcs_trex.R
@@ -3,7 +3,7 @@ fcs_trex <- function(fcs_join_obj, compare_list, reduction = c("UMAP","tSNE"), o
                      percentile_breaks = c(0,5,10,15,85,90,95,100),
                      neighbor_significance_threshold = 0.9, cluster_min_size = 50,
                      relative_cluster_distance = 30, file_output_prefix = NULL,
-                     use_MEM = TRUE, max_alloc = 200000)
+                     use_MEM = TRUE, max_alloc = 200000, plot_intensities = FALSE)
 {
   require(flowCore)
   require(ggplot2)
@@ -502,77 +502,78 @@ fcs_trex <- function(fcs_join_obj, compare_list, reduction = c("UMAP","tSNE"), o
          plot = gridExtra::arrangeGrob(grobs = listed_plots, nrow=2, ncol=1),
          device = "pdf", path = outdir, width = 10, height = 20, units = "in", dpi = 900)
 
-  intens_list <- vector("list", length = number_of_cols); names(intens_list) <- colnames(join_data)[1:number_of_cols]
-  for(i in 1:length(intens_list)) {
-    intens_list[[i]] <- cbind(join_data[,names(intens_list)[i]],map); colnames(intens_list[[i]])[1] <- names(intens_list)[i]
-  }
-  intens_pl <- function(arg1, method = "color") {
-    if(nrow(arg1)>200000){
-      arg1 <- arg1[sample(1:nrow(arg1),size=200000,replace=F),]
+  if(plot_intensities) {
+    intens_list <- vector("list", length = number_of_cols); names(intens_list) <- colnames(join_data)[1:number_of_cols]
+    for(i in 1:length(intens_list)) {
+      intens_list[[i]] <- cbind(join_data[,names(intens_list)[i]],map); colnames(intens_list[[i]])[1] <- names(intens_list)[i]
     }
-    cname1 <- colnames(arg1)[1]
-    colnames(arg1)[1] <- "col1"
-    if(method=="color") {
-      if(tolower(reduction)=="umap"){
-        plt <- ggplot(data = arg1, mapping = aes_string(x = "UMAP1", y = "UMAP2", color = colnames(arg1)[1]))
-      } else if(tolower(reduction)=="tsne") {
-        plt <- ggplot(data = arg1, mapping = aes_string(x = "tSNE1", y = "tSNE2", color = colnames(arg1)[1]))
+    intens_pl <- function(arg1, method = "color") {
+      if(nrow(arg1)>200000){
+        arg1 <- arg1[sample(1:nrow(arg1),size=200000,replace=F),]
       }
-      plt <- plt +
-        geom_point_rast(size = 0.8, pch = 19, alpha = 0.1) +
-        scale_color_viridis(option = "D") +
-        ggtitle(cname1) +
-        theme_minimal() +
-        theme(axis.title = element_blank(),
-              axis.text = element_blank(),
-              legend.title = element_blank(),
-              plot.title = element_text(hjust = 0.5),
-              legend.position = "bottom")
-    } else if(method=="fill") {
-      if(tolower(reduction)=="umap") {
-        plt <- ggplot(data = arg1, mapping = aes_string(x = "UMAP1", y = "UMAP2", fill = colnames(arg1)[1]))
-      } else if(tolower(reduction)=="tsne") {
-        plt <- ggplot(data = arg1, mapping = aes_string(x = "tSNE1", y = "tSNE2", fill = colnames(arg1)[1]))
+      cname1 <- colnames(arg1)[1]
+      colnames(arg1)[1] <- "col1"
+      if(method=="color") {
+        if(tolower(reduction)=="umap"){
+          plt <- ggplot(data = arg1, mapping = aes_string(x = "UMAP1", y = "UMAP2", color = colnames(arg1)[1]))
+        } else if(tolower(reduction)=="tsne") {
+          plt <- ggplot(data = arg1, mapping = aes_string(x = "tSNE1", y = "tSNE2", color = colnames(arg1)[1]))
+        }
+        plt <- plt +
+          geom_point_rast(size = 0.8, pch = 19, alpha = 0.1) +
+          scale_color_viridis(option = "D") +
+          ggtitle(cname1) +
+          theme_minimal() +
+          theme(axis.title = element_blank(),
+                axis.text = element_blank(),
+                legend.title = element_blank(),
+                plot.title = element_text(hjust = 0.5),
+                legend.position = "bottom")
+      } else if(method=="fill") {
+        if(tolower(reduction)=="umap") {
+          plt <- ggplot(data = arg1, mapping = aes_string(x = "UMAP1", y = "UMAP2", fill = colnames(arg1)[1]))
+        } else if(tolower(reduction)=="tsne") {
+          plt <- ggplot(data = arg1, mapping = aes_string(x = "tSNE1", y = "tSNE2", fill = colnames(arg1)[1]))
+        }
+        plt <- plt +
+          geom_point_rast(size = 0.8, pch = 21, stroke = 0.05) +
+          scale_fill_viridis(option = "D") +
+          ggtitle(cname1) +
+          theme_minimal() +
+          theme(axis.title = element_blank(),
+                axis.text = element_blank(),
+                legend.title = element_blank(),
+                plot.title = element_text(hjust = 0.5),
+                legend.position = "bottom")
       }
-      plt <- plt +
-        geom_point_rast(size = 0.8, pch = 21, stroke = 0.05) +
-        scale_fill_viridis(option = "D") +
-        ggtitle(cname1) +
-        theme_minimal() +
-        theme(axis.title = element_blank(),
-              axis.text = element_blank(),
-              legend.title = element_blank(),
-              plot.title = element_text(hjust = 0.5),
-              legend.position = "bottom")
+      return(plt)
     }
-    return(plt)
-  }
-  intens_plots <- lapply(intens_list, intens_pl)
+    intens_plots <- lapply(intens_list, intens_pl)
 
-  void_plot <- ggplot(data = cars, aes(x=speed,y=dist)) +
-    geom_point(alpha = 0) +
-    theme_void()
+    void_plot <- ggplot(data = cars, aes(x=speed,y=dist)) +
+      geom_point(alpha = 0) +
+      theme_void()
 
-  arrange_length <- ceiling(length(intens_list)/4)
-  for(i in 1:(arrange_length*4)) {
-    if(i>length(intens_plots)) {
-      intens_plots[[i]] <- void_plot
+    arrange_length <- ceiling(length(intens_list)/4)
+    for(i in 1:(arrange_length*4)) {
+      if(i>length(intens_plots)) {
+        intens_plots[[i]] <- void_plot
+      }
     }
+
+    arranged_list <- vector("list", length = arrange_length)
+    low_index <- seq(from = 1, to = arrange_length*4, by = 4)
+    high_index <- seq(from = 4, to = arrange_length*4, by = 4)
+
+    for(i in 1:length(arranged_list)) {
+      arranged_list[[i]] <- ggpubr::ggarrange(plotlist = intens_plots[low_index[i]:high_index[i]],
+                                              nrow = 2, ncol = 2)
+    }
+
+    ggsave(filename = paste0(file_output_prefix,ifelse(tolower(reduction)=="umap","UMAP","tSNE"),"_trex_heatmaps_",
+                             strftime(Sys.time(),"%Y-%m-%d_%H%M%S"),".pdf"),
+           plot = gridExtra::marrangeGrob(grobs = arranged_list, nrow=1, ncol=1, top = ""),
+           device = "pdf", path = outdir, width = 12, height = 12, units = "in", dpi = 900)
   }
-
-  arranged_list <- vector("list", length = arrange_length)
-  low_index <- seq(from = 1, to = arrange_length*4, by = 4)
-  high_index <- seq(from = 4, to = arrange_length*4, by = 4)
-
-  for(i in 1:length(arranged_list)) {
-    arranged_list[[i]] <- ggpubr::ggarrange(plotlist = intens_plots[low_index[i]:high_index[i]],
-                                            nrow = 2, ncol = 2)
-  }
-
-  ggsave(filename = paste0(file_output_prefix,ifelse(tolower(reduction)=="umap","UMAP","tSNE"),"_trex_heatmaps_",
-                           strftime(Sys.time(),"%Y-%m-%d_%H%M%S"),".pdf"),
-         plot = gridExtra::marrangeGrob(grobs = arranged_list, nrow=1, ncol=1, top = ""),
-         device = "pdf", path = outdir, width = 12, height = 12, units = "in", dpi = 900)
-
   # return(fcs_join_obj)
 }
