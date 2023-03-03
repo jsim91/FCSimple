67c50fddaeedcd5ada6d725e07ee7b289bac79f3
Joshua Simmons <joshua.d.simmons@vumc.org>
2023-03-03 15:49:00 -0600

    allow for object subsetting by cluster or source

---
+++ R/fcs_subset.R
@@ -0,0 +1,38 @@
+fcs_subset <- function(fcs_join_obj,
+                       subset_by = c("cluster","source"),
+                       subset_cluster_algorithm = NA,
+                       subset_values)
+{
+  # fcs_join_obj <- list(data = matrix(data = sample(1:1000,size=10000,replace=TRUE), ncol = 10),
+  #                      source = sample(LETTERS[1:3],10000/10,replace=T))
+  if(subset_by=="source") {
+    get_index <- which(fcs_join_obj[["source"]] %in% subset_values)
+    if(length(get_source_index)==0) {
+      stop(paste0("Value requested not found in source. Use one or more of: ",
+                  paste0(names(table(fcs_join_obj[["source"]])), collapse = ", ")))
+    }
+    # only need to subset on data and source entries then return since the other entries depend on these and should not be kept
+    fcs_join_obj[["data"]] <- fcs_join_obj[["data"]][get_index,]
+    fcs_join_obj[["source"]] <- fcs_join_obj[["source"]][get_index]
+    if("run_date" %in% names(fcs_join_obj)) {
+      fcs_join_obj[["run_date"]] <- fcs_join_obj[["run_date"]][get_index]
+    }
+  } else if(subset_by=="cluster") {
+    cluster_nums <- fcs_join_obj[[tolower(subset_cluster_algorithm)]][["clusters"]]
+    get_index <- which(cluster_nums %in% subset_values)
+    fcs_join_obj[["data"]] <- fcs_join_obj[["data"]][get_index,]
+    fcs_join_obj[["source"]] <- fcs_join_obj[["source"]][get_index]
+    if("run_date" %in% names(fcs_join_obj)) {
+      fcs_join_obj[["run_date"]] <- fcs_join_obj[["run_date"]][get_index]
+    }
+  } else {
+    stop("error in argument 'subset_by': only subettable by cluster or source")
+  }
+  fcs_join_obj <- list(data = fcs_join_obj$data,
+                       source = fcs_join_obj$source,
+                       subset_on = list(subset_by = subset_by,
+                                        subset_cluster_algorithm = subset_cluster_algorithm,
+                                        subset_values = subset_values,
+                                        source_object_indices = get_index))
+  return(fcs_join_obj)
+}
