e1b9f32c884b7810362f34fe64d6c2b4acff07ac
Joshua Simmons <joshua.d.simmons@vumc.org>
2023-03-17 11:20:00 -0500

    plot reduction diff width height

--- R/fcs_density_difference.R
+++ R/fcs_density_difference.R
@@ -163,10 +163,10 @@ fcs_plot_reduction_difference <- function(fcs_join_obj, reduction = c("UMAP","tS
 
   ggsave(filename = paste0(fname_top,".pdf"),
          plot = plt_dens_diff, device = "pdf",
-         figure_width = 8, figure_height = 8,
+         width = figure_width, height = figure_height,
          units = "in", dpi = 900, bg = "white")
   ggsave(filename = paste0(fname_bottom,".pdf"),
          plot = plt_dens_back, device = "pdf",
-         figure_width = 8, figure_height = 8,
+         width = figure_width, height = figure_height,
          units = "in", dpi = 900, bg = "white")
 }
