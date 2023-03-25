91f22537c8956d36e4d5e7637e558d7e603eb87a
Joshua Simmons <joshua.d.simmons@vumc.org>
2023-03-25 16:42:31 -0500

    tests fix outfile

--- R/fcs_tests.R
+++ R/fcs_tests.R
@@ -118,7 +118,7 @@ fcs_test_clusters <- function(fcs_join_obj, compare_list, color_list, comparison
 
   out_plots <- lapply(X = in_list, FUN = test_plot)
 
-  pdf(file = paste0(abundance_alg,"_cluster_statistics.pdf"), width = 10, height = 5)
+  pdf(file = paste0(algorithm,"_cluster_statistics.pdf"), width = 10, height = 5)
   lapply(X = out_plots, FUN = function(x) x)
   dev.off()
 }
