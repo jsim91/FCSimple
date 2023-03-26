687ad45b47c08952dc395e174837b3fc0cc2d769
Joshua Simmons <joshua.d.simmons@vumc.org>
2023-03-25 23:37:22 -0500

    allow for paired tests

--- R/fcs_tests.R
+++ R/fcs_tests.R
@@ -1,7 +1,7 @@
 fcs_test_clusters <- function(fcs_join_obj, compare_list, color_list, comparisons, denominator_cell_type,
                               algorithm = c("leiden","flowsom","louvain","phenograph","git"),
                               Rcolorbrewer_palette = "RdYlBu", # must be a colorbrewer palette that's 11 long such as Spectral or RdYlBu
-                              dot_size = 1, overlay_heatmap_numbers = TRUE)
+                              dot_size = 1, overlay_heatmap_numbers = TRUE, paired_test = FALSE)
 {
   require(ggplot2)
   require(ggpubr)
@@ -14,18 +14,40 @@ fcs_test_clusters <- function(fcs_join_obj, compare_list, color_list, comparison
   require(circlize)
 
   # testing argument assignments
-  # overlay_heatmap_numbers = TRUE
-  # denominator_cell_type = "lymphocytes"
-  # Rcolorbrewer_palette = "RdYlBu"
-  # dot_size = 1
-  # fcs_join_obj <- readRDS("J:/Mashayekhi/flow/AHA_liraglutide_scenith/lymphocytes/AHA_liraglutide_lymphocytes_fcsimple_obj_clustered.rds")
+  # cell_type <- "lymphocytes"
+  #
+  # setwd(paste0("J:/Mashayekhi/flow/AHA_liraglutide_scenith/",cell_type))
+  #
+  # fcs_obj <- readRDS("AHA_liraglutide_lymphocytes_fcsimple_obj_clustered.rds")
+  # fcs_join_obj <- fcs_obj
   # algorithm <- "leiden"
-  # abundance <- fcs_join_obj[[algorithm]][["abundance"]]
-  # compare_list <- list('2DG' = row.names(abundance)[grep(pattern = "2DG", x = row.names(abundance))],
-  #                      'DGO' = row.names(abundance)[grep(pattern = "DGO", x = row.names(abundance))],
-  #                      'No Inhib' = row.names(abundance)[grep(pattern = "No Inhib", x = row.names(abundance))])
-  # color_list <- list('2DG' = "red", 'DGO' = "blue", 'No Inhib' = "green")
-  # comparisons <- list(c('2DG','DGO'),c('DGO','No Inhib'))
+  # denominator_cell_type <- cell_type
+  # algorithm = "leiden"
+  # Rcolorbrewer_palette = "RdYlBu" # must be a colorbrewer palette that's 11 long such as Spectral or RdYlBu
+  # dot_size = 1
+  # overlay_heatmap_numbers = TRUE
+  # paired_test = TRUE
+  #
+  # abundance <- read.csv(file = list.files()[grep("^leiden_cluster_abundance",list.files())],
+  #                       check.names = FALSE, row.names = 1)
+  # abundance <- abundance[grep(pattern = "No Puro", x = row.names(abundance)),]
+  # abundance <- abundance[order(row.names(abundance)),]
+  # compare_list <- list('V1' = row.names(abundance)[grep(pattern = "v1", x = row.names(abundance))],
+  #                      'V2' = row.names(abundance)[grep(pattern = "v2", x = row.names(abundance))])
+  #
+  # differ_pattern = "v[0-9]"
+  # for(i in 1:(length(compare_list)-1)) {
+  #   if(i==1) {
+  #     next
+  #   } else {
+  #     if(mean(gsub(differ_pattern,"",compare_list[[i]])==gsub(differ_pattern,"",compare_list[[i+1]]))!=1) {
+  #       stop("Mismatching PIDs. PID order must match to do paired testing.")
+  #     }
+  #   }
+  # }
+  #
+  # color_list <- list('V1' = "red", 'V2' = "blue")
+  # comparisons <- list(c('V1','V2'))
   # end testing argument assignments
 
   rm_row <- which(!row.names(abundance) %in% unlist(compare_list))
@@ -33,21 +55,32 @@ fcs_test_clusters <- function(fcs_join_obj, compare_list, color_list, comparison
     abundance <- abundance[-rm_row,]
   }
 
-  group_assignment <- rep(NA,nrow(abundance))
+  rbind_list <- vector("list", length = length(compare_list))
   for(i in 1:length(compare_list)) {
-    group_assignment[which(row.names(abundance) %in% compare_list[[i]])] <- names(compare_list)[i]
+    rbind_list[[i]] <- t(t(abundance)[,compare_list[[i]]])
+    rbind_list[[i]] <- cbind(rbind_list[[i]],data.frame(group = as.character(1:nrow(rbind_list[[i]]))))
   }
+
+  group_assignment <- c()
+  for(i in 1:length(compare_list)) {
+    group_assignment <- append(group_assignment,rep(names(compare_list)[i],length(compare_list[[i]])))
+  }
+
+  abundance <- do.call(rbind,rbind_list)
+  group_vals <- abundance[,"group"]
+  abundance <- abundance[,-which(colnames(abundance)=="group")]
 
   cluster_list <- vector("list", length = ncol(abundance))
   names(cluster_list) <- colnames(abundance)
   for(i in 1:length(cluster_list)) {
-    tmp_df <- data.frame(PID = row.names(abundance), clus = abundance[,names(cluster_list)[i]])
-    colnames(tmp_df)[2] <- paste0("cluster_",names(cluster_list)[i])
+    tmp_df <- data.frame(cluster = rep(colnames(abundance)[i],nrow(abundance)),
+                         pid = row.names(abundance), group = group_vals,
+                         frequency = abundance[,i])
     tmp_df$compare_group <- group_assignment
     cluster_list[[i]] <- tmp_df
   }
 
-  test_out <- vector("list", length = length(cluster_list)); names(test_out) <- names(cluster_list)
+  # test_out <- vector("list", length = length(cluster_list)); names(test_out) <- names(cluster_list)
   plot_cols <- unlist(color_list)
   my_compare <- comparisons
 
@@ -61,8 +94,9 @@ fcs_test_clusters <- function(fcs_join_obj, compare_list, color_list, comparison
                         backmat = hm_tiles, use_palette = Rcolorbrewer_palette,
                         size_of_dots = dot_size, cell_type_denom = denominator_cell_type,
                         heatmap_overlay_values = overlay_heatmap_numbers,
-                        abundance_alg = algorithm) {
+                        abundance_alg = algorithm, pair_test = paired_test) {
 
+    # testing
     # abundance_alg <- algorithm
     # heatmap_overlay_values = overlay_heatmap_numbers
     # size_of_dots = dot_size
@@ -70,6 +104,7 @@ fcs_test_clusters <- function(fcs_join_obj, compare_list, color_list, comparison
     # input <- in_list[[1]]
     # compare_these = my_compare; dplot_col = plot_cols
     # use_palette = Rcolorbrewer_palette
+    # pair_test = paired_test
 
     plot_input <- input[[1]]
     hm_input <- input[[2]]
@@ -82,12 +117,18 @@ fcs_test_clusters <- function(fcs_join_obj, compare_list, color_list, comparison
     colnames(plot_input)[cluster_col_ind] <- "placeholder"
 
     plt <- ggplot(data = plot_input, mapping = aes(x = group_assignment, y = placeholder, color = group_assignment)) +
-      geom_boxplot(fill = "#bfbfbf", lwd = 0.5, alpha = 0.4, width = 0.4) +
-      geom_dotplot(data = plot_input, aes(fill = group_assignment), color = "black", stroke = 1,
-                   binaxis = "y", stackdir = "center", position = "dodge", binpositions="all",
-                   dotsize = size_of_dots) +
-      stat_compare_means(method = "wilcox", comparisons = compare_these, size = 4.5, paired = FALSE) +
-      scale_y_continuous(expand = expansion(mult = c(0.05, 0.15))) +
+      geom_boxplot(fill = "#bfbfbf", lwd = 0.5, alpha = 0.4, width = 0.4)
+    if(pair_test) {
+      plt <- plt + geom_line(mapping = aes(group = group), color = "black") +
+        geom_point(mapping = aes(fill = compare_group), pch = 21, size = size_of_dots*3, color = "black") +
+        stat_compare_means(method = "wilcox", comparisons = compare_these, size = 4.5, paired = TRUE)
+    } else {
+      plt <- plt + geom_dotplot(data = plot_input, aes(fill = group_assignment), color = "black", stroke = 1,
+                                binaxis = "y", stackdir = "center", position = "dodge", binpositions="all",
+                                dotsize = size_of_dots) +
+        stat_compare_means(method = "wilcox", comparisons = compare_these, size = 4.5, paired = FALSE)
+    }
+    plt <- plt + scale_y_continuous(expand = expansion(mult = c(0.05, 0.15))) +
       labs(y = paste0("% of ",cell_type_denom), title = gsub("_"," ",capture_cluster)) +
       scale_fill_manual(values = dplot_col) +
       scale_color_manual(values = dplot_col) +
@@ -118,11 +159,8 @@ fcs_test_clusters <- function(fcs_join_obj, compare_list, color_list, comparison
 
   out_plots <- lapply(X = in_list, FUN = test_plot)
 
-  # pdf(file = paste0(algorithm,"_cluster_statistics.pdf"), width = 10, height = 5)
-  # lapply(X = out_plots, FUN = function(x) x)
-  # dev.off()
-
   fcs_join_obj[[algorithm]][["cluster_test_results"]] <- out_plots
 
+  print(paste0("test results saved in object under: fcs_join_obj[['",algorithm,"']][['cluster_test_results']]"))
   return(fcs_join_obj)
 }
