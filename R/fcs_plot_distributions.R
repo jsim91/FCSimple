af8854aefeb2f1669818a14b7477438ea3345d6d
Joshua Simmons <joshua.d.simmons@vumc.org>
2023-04-05 07:27:08 -0500

    plot distr function fix

--- R/fcs_plot_distributions.R
+++ R/fcs_plot_distributions.R
@@ -11,15 +11,13 @@ fcs_plot_distribution <- function(fcs_join_obj,
   require(ggridges)
 
   obj_data <- as.data.frame(fcs_join_obj[["data"]])
-  if(separate_by=="date") {
+  if(tolower(separate_by)=="date") {
     if(!"run_date" %in% names(fcs_join_obj)){
       print("Unable to find run date. Using separate_by = 'none' instead.")
     } else {
       batch <- fcs_join_obj[["run_date"]]
       obj_data$date <- batch
     }
-  }
-  if("date" %in% colnames(obj_data)) {
     data_split <- vector("list", length = ncol(obj_data)-1)
     names(data_split) <- colnames(obj_data)[1:(ncol(obj_data)-1)]
     for(i in 1:length(data_split)) {
@@ -27,15 +25,13 @@ fcs_plot_distribution <- function(fcs_join_obj,
                                     batch = obj_data[,"date"])
       colnames(data_split[[i]])[1] <- colnames(obj_data)[i]
     }
-  } else {
+  } else if(tolower(separate_by) == "cluster") {
     data_split <- vector("list", length = ncol(obj_data))
     names(data_split) <- colnames(obj_data)
     for(i in 1:length(data_split)) {
       data_split[[i]] <- data.frame(val1 = obj_data[,which(colnames(obj_data)==names(data_split)[i])])
       colnames(data_split[[i]])[1] <- colnames(obj_data)[i]
     }
-  }
-  if(tolower(separate_by) == "cluster") {
     if(!tolower(plot_algorithm) %in% c("leiden","flowsom","louvain","phenograph","git")) {
       stop(paste0("error in argument 'plot_algorithm': clusters not found for ",tolower(plot_algorithm)," not found in 'fcs_join_obj'"))
     }
@@ -43,14 +39,20 @@ fcs_plot_distribution <- function(fcs_join_obj,
       data_split[[i]]$cluster <- factor(fcs_join_obj[[tolower(plot_algorithm)]]$clusters)
     }
   }
-  if(separate_by == "none") {
+  if(tolower(separate_by) == "none") {
+    data_split <- vector("list", length = ncol(obj_data))
+    names(data_split) <- colnames(obj_data)
+    for(i in 1:length(data_split)) {
+      data_split[[i]] <- data.frame(val1 = obj_data[,which(colnames(obj_data)==names(data_split)[i])])
+      colnames(data_split[[i]])[1] <- colnames(obj_data)[i]
+    }
     plot_set <- lapply(X = data_split, FUN = plot_none, rm0 = rm_zero, trq = trim_quantile)
     ggsave(filename = paste0(outdir,"/panel_distributions_concatenation_",strftime(Sys.time(),"%Y-%m-%d_%H%M%S"),".pdf"),
            plot = ggpubr::ggarrange(plotlist = plot_set, nrow = ceiling(sqrt(length(plot_set))),
                                     ncol = ceiling(sqrt(length(plot_set)))), device = "pdf",
            width = ceiling(sqrt(length(plot_set)))*2.5, height = ceiling(sqrt(length(plot_set)))*2.5, units = "in",
            dpi = 900, limitsize = FALSE)
-  } else if(separate_by == "date") {
+  } else if(tolower(separate_by) == "date") {
     if(!"batch" %in% colnames(data_split[[1]])) {
       stop("error in argument 'separate_by': no run date found, cannot plot by run date/batch")
     }
@@ -60,7 +62,7 @@ fcs_plot_distribution <- function(fcs_join_obj,
                                     ncol = ceiling(sqrt(length(plot_set))), legend = "bottom", common.legend = TRUE),
            device = "pdf", width = ceiling(sqrt(length(plot_set)))*2.5, height = ceiling(sqrt(length(plot_set)))*2.5,
            units = "in", dpi = 900, limitsize = FALSE)
-  } else if(separate_by == "cluster") {
+  } else if(tolower(separate_by) == "cluster") {
     plot_set <- lapply(X = data_split, FUN = plot_cluster, rm0 = rm_zero, trq = trim_quantile)
     ggsave(filename = paste0(outdir,"/panel_distributions_by_cluster_",strftime(Sys.time(),"%Y-%m-%d_%H%M%S"),".pdf"),
            plot = ggpubr::ggarrange(plotlist = plot_set, nrow = ceiling(sqrt(length(plot_set))),
