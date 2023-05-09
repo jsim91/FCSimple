13d78cc47aebdfd49e1ab2e7baa054160c3aab6b
Joshua Simmons <joshua.d.simmons@vumc.org>
2023-05-09 05:11:44 -0500

    fcs_tests spell error fix

--- R/fcs_tests.R
+++ R/fcs_tests.R
@@ -58,7 +58,7 @@ fcs_test_clusters <- function(fcs_join_obj, compare_list, color_list, comparison
   if(length(rm_row)!=0) {
     abundance <- abundance[-rm_row,]
   }
-  for(i in 1:length(comapare_list)) {
+  for(i in 1:length(compare_list)) {
     try_rm <- which(!compare_list[[i]] %in% row.names(abundance))
     if(length(try_rm)!=0) {
       compare_list[[i]] <- compare_list[[i]][-try_rm]
