9b3c7b0cc8e62b0d20f415a26147a3091b08229b
Joshua Simmons <joshua.d.simmons@vumc.org>
2023-05-24 12:29:31 -0500

    write file corrections

--- R/fcs_write_file.R
+++ R/fcs_write_file.R
@@ -23,19 +23,40 @@ fcs_write.FCS <- function(fcs_join_obj,
   fcs_include[[1]] <- data_incl
 
   if(length(include_reductions)!=0) {
+    hold_reductions <- vector("list", length = length(include_reductions))
     for(i in 1:length(include_reductions)) {
       if(tolower(include_reductions[i]) %in% names(fcs_join_obj)) {
-        append_coords <- as.matrix(fcs_join_obj[[tolower(include_reductions[i])]][["coordinates"]])
-        if(i==1) {
-          data_return_1 <- append_coords
-        } else {
-          data_return_1 <- cbind(data_return_1, append_coords)
-        }
+        hold_reductions[[i]] <- as.matrix(fcs_join_obj[[tolower(include_reductions[i])]][["coordinates"]])
       } else {
         warning(paste0("results of reduction ",tolower(include_reductions[i])," not found in object and will not be included"))
+        hold_reductions[[i]] <- NULL
       }
     }
-    fcs_include[[2]] <- data_return_1
+    rm_reduction <- c()
+    for(i in 1:length(hold_reductions)) {
+      if(is.null(hold_reductions[[i]])) {
+        rm_reduction <- append(rm_reduction, i)
+      }
+    }
+    if(length(rm_reduction)!=0) {
+      if(length(rm_reduction)==1) {
+        pared_reduction <- hold_reductions[[-rm_reduction]]
+      } else if(length(rm_reduction)>1) {
+        pared_reduction <- hold_reductions[-rm_reduction]
+      }
+    } else if(length(rm_reduction)==0) {
+      pared_reduction <- hold_reductions
+    }
+    for(i in 1:length(pared_reduction)) {
+      if(i==1) {
+        add_reduction <- pared_reduction[[i]]
+      } else {
+        add_reduction <- cbind(add_reduction, pared_reduction[[i]])
+      }
+    }
+    fcs_include[[2]] <- as.matrix(add_reduction)
+  } else {
+    fcs_include[[2]] <- NULL
   }
   if(length(include_clusterings)!=0) {
     cluster_numbers <- vector("list", length = length(include_clusterings))
@@ -45,58 +66,58 @@ fcs_write.FCS <- function(fcs_join_obj,
         cluster_numbers[[i]] <- fcs_join_obj[[tolower(include_clusterings[i])]][["clusters"]]
       } else {
         warning(paste0("results of clustering ",tolower(include_clusterings[i])," not found in object and will not be included"))
+        cluster_numbers[[i]] <- NULL
       }
     }
-    rm_elements <- c()
+    rm_cluster <- c()
     for(i in 1:length(cluster_numbers)) {
-      if(length(cluster_numbers[[i]])==0) {
-        rm_elements <- append(rm_elements,i)
+      if(is.null(cluster_numbers[[i]])) {
+        rm_reduction <- append(rm_reduction, i)
       }
     }
-    if(length(rm_elements)!=0) {
-      if(length(rm_elements)==1) {
-        cluster_numbers[[rm_elements]] <- NULL
-      } else {
-        cluster_numbers[rm_elements] <- NULL
+    if(length(rm_reduction)!=0) {
+      if(length(rm_reduction)==1) {
+        pared_cluster <- cluster_numbers[[-rm_reduction]]
+      } else if(length(rm_reduction)>1) {
+        pared_cluster <- cluster_numbers[-rm_reduction]
       }
+    } else if(length(rm_reduction)==0) {
+      pared_cluster <- cluster_numbers
     }
-    for(i in 1:length(cluster_numbers)) {
-      append_numbers <- as.matrix(data.frame(var1 = cluster_numbers[[i]]))
-      colnames(append_numbers) <- names(cluster_numbers)[i]
+    for(i in 1:length(pared_cluster)) {
       if(i==1) {
-        data_return_2 <- append_numbers
+        tmp_cluster_mat <- data.frame(var1 = pared_cluster[[i]])
+        colnames(tmp_cluster_mat)[1] <- paste0(names(pared_clusters)[i],"_cluster")
+        add_cluster <- tmp_cluster_mat
       } else {
-        data_return_2 <- cbind(data_return_2, append_numbers)
+        tmp_cluster_mat <- data.frame(var1 = pared_cluster[[i]])
+        colnames(tmp_cluster_mat)[1] <- paste0(names(pared_clusters)[i],"_cluster")
+        add_cluster <- cbind(add_cluster, tmp_cluster_mat)
       }
     }
-    fcs_include[[3]] <- data_return_2
+    fcs_include[[3]] <- as.matrix(add_cluster)
+  } else {
+    fcs_include[[3]] <- NULL
   }
-  which_populated <- which(sapply(fcs_include, function(x) return(class(x)[1]))!="NULL")
-  if(length(which_populated)<3) {
-    if(length(which_populated)==1) {
-      final_return <- fcs_include[[which_populated]]
-    } else if(length(which_populated)==2) {
-      fcs_include[which(!1:3 %in% which_populated)] <- NULL
-      final_return <- cbind(fcs_include[[1]], fcs_include[[2]])
-    }
-  } else if(length(which_populated)==3) {
+  which_populated <- as.numeric(which(sapply(fcs_include, function(arg1) return(!is.null(arg1)))))
+  if(length(which_populated)==0) {
+    stop("unable to return any values.. verify that the input object has a 'data' entry")
+  } else if(length(which_populated)==1) {
+    final_return <- fcs_include[[which_populated]]
+  } else if(length(which_populated)>1) {
     for(i in 1:length(which_populated)) {
       if(i==1) {
-        next
-      } else if(i==2) {
-        final_return <- cbind(fcs_include[[1]], fcs_include[[i]])
+        final_return <- fcs_include[[which_populated[[i]]]]
       } else {
-        final_return <- cbind(final_return, fcs_include[[i]])
+        final_return <- cbind(final_return, fcs_include[[which_populated[[i]]]])
       }
     }
   }
-  if(class(final_return)[1]!="matrix") {
-    final_return <- as.matrix(final_return)
-  }
+  final_return <- as.matrix(final_return) # structure should be a matrix, but as.matrix is used just in case
   out_ff <- new("flowFrame", exprs = final_return)
   if(include_timestamp) {
-    write.FCS(x = out_ff, filename = paste0(gsub("\\/$","",outdir),"/",fcs_name,"_",strftime(Sys.time(),"%Y-%m-%d_%H%M%S"),".fcs"))
+    write.FCS(x = out_ff, filename = paste0(gsub("\\/$","",outdir),"/",gsub("\\.fcs$","",fcs_name),"_",strftime(Sys.time(),"%Y-%m-%d_%H%M%S"),".fcs"))
   } else {
-    write.FCS(x = out_ff, filename = paste0(gsub("\\/$","",outdir),"/",fcs_name,".fcs"))
+    write.FCS(x = out_ff, filename = paste0(gsub("\\/$","",outdir),"/",gsub("\\.fcs$","",fcs_name),".fcs"))
   }
 }
