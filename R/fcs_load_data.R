78aea8f7cfdc978866b65a2288448fb0fdca24b3
Joshua Simmons <joshua.d.simmons@vumc.org>
2024-02-04 08:31:08 -0600

    vignette update

--- R/fcs_load_data.R
+++ R/fcs_load_data.R
@@ -1,5 +1,5 @@
 fcs_load_data <- function() {
-  package_dir <- gsub("\\/","",system.file(package = "FCSimple"))
+  package_dir <- gsub("\\/$","",system.file(package = "FCSimple"))
   data_loc <- paste0(package_dir,"/data")
   fcs_fil <- list.files(path = data_loc, pattern = "fcs$", full.names = TRUE, include.dirs = TRUE)
   return(fcs_fil)
