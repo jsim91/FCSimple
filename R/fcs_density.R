334bc868511240a3a4144b8624597ae9016fb596
Joshua Simmons <joshua.d.simmons@vumc.org>
2024-04-12 12:18:42 -0500

    return legend with plot as list from fcs_plot_reduction_density

--- R/fcs_density.R
+++ R/fcs_density.R
@@ -11,6 +11,8 @@ fcs_plot_reduction_density <- function(fcs_join_obj, reduction = "UMAP", n_kde =
   # require(scales)
   # require(RColorBrewer)
   # require(metR)
+  # require(grid)
+  # require(ggpubr)
   library(ggplot2)
 
   # fcs_join_obj = readRDS("J:/tmp_JO/CD4_leiden_umap.rds")
@@ -22,7 +24,8 @@ fcs_plot_reduction_density <- function(fcs_join_obj, reduction = "UMAP", n_kde =
   # text_size_factor = 1
   # title_string = "my title"
   # n_color_steps = 15
-  # RColorBrewer_pal = "Spectral"
+  # # RColorBrewer_pal = "Spectral"
+  # RColorBrewer_pal = "RdYlBu"
   # plot_border_thickness = 1
 
   if(xlimits[1]=="auto") {
@@ -79,56 +82,25 @@ fcs_plot_reduction_density <- function(fcs_join_obj, reduction = "UMAP", n_kde =
     }
   }
 
-  # scale_fill_craftfermenter <- function(..., type = "seq", palette = 1, direction = -1, na.value = "white", guide = "coloursteps", aesthetics = "fill") {
-  #   type <- match.arg(type, c("seq", "div", "qual"))
-  #   if (type == "qual") {
-  #     warn("Using a discrete colour palette in a binned scale.\n  Consider using type = \"seq\" or type = \"div\" instead")
-  #   }
-  #   ggplot2::binned_scale(aesthetics, palette = "fermenter", ggplot2:::binned_pal(craftbrewer_pal(type, palette, direction)), na.value = na.value, guide = guide, ...)
-  # }
-
   scale_fill_craftfermenter <- function(..., type = "seq", palette = 1, direction = -1, na.value = "white", guide = "coloursteps", aesthetics = "fill", stepnum = n_color_steps) {
     type <- match.arg(type, c("seq", "div", "qual"))
     if (type == "qual") {
       warn("Using a discrete colour palette in a binned scale.\n  Consider using type = \"seq\" or type = \"div\" instead")
     }
-    fn <- craftbrewer_pal(type = "seq", palette = "Spectral", direction = -1)
+    fn <- craftbrewer_pal(type = "seq", palette = RColorBrewer_pal, direction = -1)
     colors <- fn(stepnum)
     ggplot2::scale_fill_gradientn(colours = colors, na.value = na.value, guide = guide, ...)
   }
 
   diff12.m.copy <- diff12.m
   mybreaks <- seq(from = min(200*diff12.m$z), to = max(200*diff12.m$z), length.out = n_color_steps)
-  # custom_palette <- c("#FFFFFF", RColorBrewer::brewer.pal(10,"Spectral"))
-  #
-  # # ggplot2::ggplot(ggplot2::faithfuld, ggplot2::aes(y=eruptions, x=waiting, z=100*density)) +
-  #   # ggplot2::geom_contour_filled(breaks = breaks) +
-  # ggplot2::ggplot(data = diff12.m, mapping = ggplot2::aes(Var1, Var2, z = 200*z)) +
-  #   ggplot2::geom_contour_filled() +
-  #   ggplot2::scale_fill_distiller(super = metR::ScaleDiscretised, palette = custom_palette) +
-  #   ggplot2::geom_contour(data = diff12.m.copy, mapping = ggplot2::aes(Var1, Var2, z = 200*z), color = "black") +
-  #   ggplot2::theme(legend.position = "bottom") +
-  #   ggplot2::coord_cartesian(xlim = dim1_range, ylim = dim2_range, expand = FALSE) +
-  #   ggplot2::labs(x = reduction_names[1], y = reduction_names[2], title = title_string)
-  #
-  #
-  # custom_palette <- c("#FFFFFF", rev(RColorBrewer::brewer.pal(11,"Spectral")), "#e0023a")
-  #
-  # ggplot2::ggplot(data = diff12.m, mapping = ggplot2::aes(Var1, Var2, z = 200*z)) +
-  #   ggplot2::geom_contour_filled() +
-  #   ggplot2::scale_fill_manual(values = custom_palette) +
-  #   ggplot2::geom_contour(data = diff12.m.copy, mapping = ggplot2::aes(Var1, Var2, z = 200*z), color = "black") +
-  #   ggplot2::theme(legend.position = "bottom") +
-  #   ggplot2::coord_cartesian(xlim = dim1_range, ylim = dim2_range, expand = FALSE) +
-  #   ggplot2::labs(x = reduction_names[1], y = reduction_names[2], title = title_string)
-
 
   plt <- ggplot2::ggplot() +
     metR::geom_contour_fill(data = diff12.m, mapping = ggplot2::aes(Var1, Var2, z = z_inflation*z)) +
     scale_fill_craftfermenter(
       # breaks = mybreaks,
-      palette = "Spectral",#RColorBrewer_pal,
-      guide = guide_colorsteps(
+      palette = RColorBrewer_pal,#RColorBrewer_pal,
+      guide = ggplot2::guide_colorsteps(
         frame.colour = "black",
         ticks.colour = "black",
         barwidth=20,
@@ -145,15 +117,41 @@ fcs_plot_reduction_density <- function(fcs_join_obj, reduction = "UMAP", n_kde =
                      axis.title = ggplot2::element_blank(),
                      axis.ticks = ggplot2::element_blank(),
                      legend.title = ggplot2::element_blank(),
-                     legend.position = "none",
+                     legend.position = "bottom",
                      panel.border = ggplot2::element_rect(color = "black", linewidth = plot_border_thickness))
   } else {
     plt <- plt + ggplot2::theme(axis.text = ggplot2::element_blank(),
                                 plot.title = ggplot2::element_text(hjust = 0.5, size = 28*text_size_factor),
                                 axis.title = ggplot2::element_blank(),
                                 axis.ticks = ggplot2::element_blank(),
-                                legend.title = ggplot2::element_blank(),
-                                legend.position = "none")
+                                legend.position = "bottom",
+                                legend.title = ggplot2::element_blank())
   }
-  return(plt)
+
+  build_plt <- ggplot2::ggplot_build(plot = plt)
+  ucol <- unique(build_plt[["data"]][[1]][["fill"]])
+  ucol <- ucol[-1]
+
+  plt_leg <- ggplot2::ggplot(data.frame(x = c(0, 1), y = c(0, 1), col = c(0, 1)), ggplot2::aes(x = x, y = y, fill = col)) +
+    ggplot2::geom_tile() +
+    ggplot2::scale_fill_gradientn(colors = ucol,
+                         breaks = c(0, 1),
+                         labels = c("low density", "high density")) +
+    ggplot2::theme_void() +
+    ggplot2::guides(fill = ggplot2::guide_colorbar(title = ggplot2::element_blank(),
+                                 label.position = "bottom",
+                                 title.position = "top",
+                                 direction = "horizontal",
+                                 position = "bottom",
+                                 title.hjust = 0.5,
+                                 label.hjust = 0.5,
+                                 barwidth = 20,
+                                 draw.ulim = F,
+                                 draw.llim = F,
+                                 barheight = 1.5)) +
+    ggplot2::theme(legend.text = ggplot2::element_text(size = 20*text_size_factor),
+                   legend.frame = ggplot2::element_rect(linewidth = 0.5))
+  get_leg <- ggpubr::get_legend(plt_leg, "fill")
+
+  return(list(plot = plt, legend = get_leg))
 }
