eb7180b7d5389299ed6bd69a7e1287aa15bdedfc
Joshua Simmons <joshua.d.simmons@vumc.org>
2024-07-05 04:40:11 -0500

    allow for harmony batch correction

--- R/fcs_batch_correction.R
+++ R/fcs_batch_correction.R
@@ -1,6 +1,6 @@
-fcs_batch_correction <- function(fcs_join_obj, use_rep = "data", correction_method = c("cyCombine"),
+fcs_batch_correction <- function(fcs_join_obj, use_rep = "data", correction_method = c("cyCombine", "harmony"),
                                  correction_markers = "all", batch_source_regex = "[0-9]+\\-[A-Za-z]+\\-[0-9]+",
-                                 cyCombine_SOMx = 8, cyCombine_SOMy = 8)
+                                 cyCombine_SOMx = 8, cyCombine_SOMy = 8, cyCombine_detect_effects = FALSE)
 {
   # only cyCombine supported for now
   use_rep <- tolower(use_rep)
@@ -35,35 +35,53 @@ fcs_batch_correction <- function(fcs_join_obj, use_rep = "data", correction_meth
     exp_data$samples <- fcs_join_obj$source
     exp_data$batch <- stringr::str_extract(string = fcs_join_obj$source, pattern = batch_source_regex)
     print(paste0(length(unique(exp_data$batch))," batches found for correction: ",paste0(unique(exp_data$batch),collapse = ", ")))
-    print("...detecting batch effects before correction...")
-    cyCombine::detect_batch_effect(df = exp_data, out_dir = batch_correction_outdir, norm_method = "scale",
-                                   xdim = cyCombine_SOMx, ydim = cyCombine_SOMy, seed = 123, batch_col = "batch",
-                                   markers = marks)
-    print(paste0("Consider reviewing cyCombine results here before proceeding beyond batch correction: ",cyc_outdir1))
+    if(cyCombine_detect_effects) {
+      print("...detecting batch effects before correction...")
+      cyCombine::detect_batch_effect(df = exp_data, out_dir = batch_correction_outdir, norm_method = "scale",
+                                     xdim = cyCombine_SOMx, ydim = cyCombine_SOMy, seed = 123, batch_col = "batch",
+                                     markers = marks)
+      print(paste0("Consider reviewing cyCombine results here before proceeding beyond batch correction: ",cyc_outdir1))
+    }
     print("...normalizing data...")
     norm_data <- cyCombine::normalize(df = exp_data, markers = marks, norm_method = "scale")
     print("...creating som...")
     labels <- cyCombine::create_som(df = norm_data, markers = marks, rlen = 10, seed = 123)
     print("...correcting data... with anchor = NULL")
     corrected <- cyCombine::correct_data(df = exp_data, label = labels, markers = marks, covar = "batch")
-    print("...deteching batch effects after correction...")
-    cyCombine::detect_batch_effect(df = corrected, out_dir = cyc_outdir2, norm_method = "scale",
-                                   xdim = cyCombine_SOMx, ydim = cyCombine_SOMy, seed = 123, batch_col = "batch",
-                                   markers = marks)
+    if(cyCombine_detect_effects) {
+      print("...detecting batch effects after correction...")
+      cyCombine::detect_batch_effect(df = corrected, out_dir = cyc_outdir2, norm_method = "scale",
+                                     xdim = cyCombine_SOMx, ydim = cyCombine_SOMy, seed = 123, batch_col = "batch",
+                                     markers = marks)
+    }
     corrected_val <- as.data.frame(corrected)
     exprs_data_corrected <- corrected_val[,marks]
     corrected_source <- corrected_val$sample
     print("saving correction history in $batch_correction")
     fcs_join_obj[['batch_correction']] <- list(data = exprs_data_corrected,
                                                source = corrected_source,
-                                               method = "cyCombine", list(markers_corrected = marks,
-                                                                          batches_corrected = unique(exp_data$batch),
-                                                                          norm_method = "scale",
-                                                                          seed = 123,
-                                                                          SOMx_dim = cyCombine_SOMx,
-                                                                          SOMy_dim = cyCombine_SOMy,
-                                                                          datetime = Sys.time(),
-                                                                          session_info = sessionInfo()))
+                                               method = "cyCombine",
+                                               other = list(markers_corrected = marks,
+                                                            batches_corrected = unique(exp_data$batch),
+                                                            norm_method = "scale",
+                                                            seed = 123,
+                                                            SOMx_dim = cyCombine_SOMx,
+                                                            SOMy_dim = cyCombine_SOMy,
+                                                            datetime = Sys.time(),
+                                                            session_info = sessionInfo()))
+  } else if(correction_method[1]=="harmony") {
+    require(harmony)
+    harm_in <- as.matrix(rep_data)
+    print(paste0("batches found for harmony correction: ", paste0(unique(fcs_join_obj[["run_date"]]), collapse = ", ")))
+    harm_meta <- data.frame(cell_id = 1:nrow(harm_in), batch = fcs_join_obj[["run_date"]])
+    harm_out <- harmony::RunHarmony(data_mat = harm_in, meta_data = harm_meta, vars_use = "batch")
+    fcs_join_obj[['batch_correction']] <- list(data = harm_out,
+                                               harmony_meta = harm_meta,
+                                               method = "harmony",
+                                               other = list(number_of_batches = length(unique(harm_meta$batch)),
+                                                            batches = unique(harm_meta$batch),
+                                                            datetime = Sys.time(),
+                                                            session_info = sessionInfo()))
   }
   if(!'object_history' %in% names(fcs_join_obj)) {
     print("Consider running FCSimple::fcs_update() on the object.")
