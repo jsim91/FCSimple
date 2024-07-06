62ccdf08239d46509bb2f4c029d79f434f5e8579
jsim91 <60735978+jsim91@users.noreply.github.com>
2024-07-06 13:26:43 -0500

    Create transf_hyperlog.py

---
+++ inst/python/transf_hyperlog.py
@@ -0,0 +1,33 @@
+import flowkit as fk
+import numpy as np
+import pandas as pd
+import sys
+import os
+
+in_file = sys.argv[1]
+out_file = sys.argv[2]
+in_param = sys.argv[3
+in_t = float(sys.argv[4])
+in_w = float(sys.argv[5])
+in_m = float(sys.argv[6])
+in_a = float(sys.argv[7])
+
+data = pd.read_csv(in_file, index_col = 0)
+
+try:
+    os.remove(in_file)
+except OSError:
+    pass
+
+hyperlog_xform = fk.transforms.HyperlogTransform(
+    'hyperlog',
+    param_t=in_t,
+    param_w=in_w,
+    param_m=in_m,
+    param_a=in_a
+)
+
+sample_from_df = fk.Sample(data, sample_id='my_sample_from_dataframe')
+sample_from_df.apply_transform(hyperlog_xform)
+df_events = sample_from_df.as_dataframe(source='xform')
+df_events.to_csv(out_file + '/__tmp_exprs__.csv', index = False)
