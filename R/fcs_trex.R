c5c12b2cd4bf0c6f595a8a8000148458a32bfa79
Joshua Simmons <joshua.d.simmons@vumc.org>
2024-07-26 13:09:31 -0500

    asdfadf

--- R/fcs_trex.R
+++ R/fcs_trex.R
@@ -383,48 +383,49 @@ fcs_trex <- function(fcs_join_obj, compare_list, reduction = c("UMAP","tSNE"), o
   }
   write.csv(x = freq_mat, file = file.path(outdir,paste0(file_output_prefix,"trex_significant_cluster_frequencies_",
                                                          strftime(Sys.time(),"%Y-%m-%d_%H%M%S"),".csv")), row.names = TRUE)
+
+  require(CATALYST)
+  require(ComplexHeatmap)
+  require(circlize)
+  require(grid)
+  require(RColorBrewer)
+
+  number_of_cols <- ncol(fcs_join_obj[["data"]])
+  event_sources = 1:nrow(clustered_data)
+  cluster_numbers = clustered_data$cluster
+  include_channels = colnames(clustered_data)[1:number_of_cols]
+  heatmap_data = clustered_data[,1:number_of_cols]
+
+  scaled.global <- CATALYST:::.scale_exprs(t(heatmap_data[,include_channels]), 1, 0.01)
+  global.t <- t(scaled.global)
+  backend.matrix <- matrix(data=NA,nrow=length(unique(cluster_numbers)),ncol=ncol(global.t))
+  cluster_numbers <- as.character(cluster_numbers)
+  row.names(backend.matrix) <- unique(cluster_numbers)[order(unique(cluster_numbers))]
+  colnames(backend.matrix) <- colnames(global.t)
+  for(i in 1:nrow(backend.matrix)) {
+    get.clus <- which(cluster_numbers==row.names(backend.matrix)[i])
+    for(j in 1:ncol(backend.matrix)){
+      backend.matrix[i,j] <- median(global.t[get.clus,j])
+    }
+  }
+  hm_pal = rev(RColorBrewer::brewer.pal(11, "RdYlBu"))
+  z <- backend.matrix
+  color.map.fun = circlize::colorRamp2(seq(min(z),max(z), l = n <- 100), colorRampPalette(hm_pal)(n))
+  ncell <- rep(NA,times=length(unique(cluster_numbers)))
+  names(ncell) <- unique(cluster_numbers)[order(unique(cluster_numbers))]
+  for(i in 1:length(ncell)) {
+    ncell[i] <- sum(cluster_numbers==names(ncell)[i])
+  }
+  pop.freq <- matrix(data=ncell,ncol=1)
+  row.names(pop.freq) <- names(ncell)
+  size_anno_nums <- round((pop.freq/sum(pop.freq))*100,2)
+  ranno1 <- rowAnnotation(`Cluster\nSize`=anno_barplot(pop.freq,border=F,width=unit(1.75, "cm"),
+                                                       axis_param=list(gp=gpar(fontsize=9)), axis = TRUE),
+                          annotation_name_gp=gpar(fontsize=10,fontface="bold"), name = "Cluster\nSize")
+  ranno2 <- rowAnnotation(frequency=anno_text(paste0(size_anno_nums,"%"),
+                                              gp=gpar(fontsize=10,fontface="bold")))
+  backend.matrix <- backend.matrix[order(row.names(backend.matrix)),]
   if(length(uclus)<=1) {
-    require(CATALYST)
-    require(ComplexHeatmap)
-    require(circlize)
-    require(grid)
-    require(RColorBrewer)
-
-    number_of_cols <- ncol(fcs_join_obj[["data"]])
-    event_sources = 1:nrow(clustered_data)
-    cluster_numbers = clustered_data$cluster
-    include_channels = colnames(clustered_data)[1:number_of_cols]
-    heatmap_data = clustered_data[,1:number_of_cols]
-
-    scaled.global <- CATALYST:::.scale_exprs(t(heatmap_data[,include_channels]), 1, 0.01)
-    global.t <- t(scaled.global)
-    backend.matrix <- matrix(data=NA,nrow=length(unique(cluster_numbers)),ncol=ncol(global.t))
-    cluster_numbers <- as.character(cluster_numbers)
-    row.names(backend.matrix) <- unique(cluster_numbers)[order(unique(cluster_numbers))]
-    colnames(backend.matrix) <- colnames(global.t)
-    for(i in 1:nrow(backend.matrix)) {
-      get.clus <- which(cluster_numbers==row.names(backend.matrix)[i])
-      for(j in 1:ncol(backend.matrix)){
-        backend.matrix[i,j] <- median(global.t[get.clus,j])
-      }
-    }
-    hm_pal = rev(RColorBrewer::brewer.pal(11, "RdYlBu"))
-    z <- backend.matrix
-    color.map.fun = circlize::colorRamp2(seq(min(z),max(z), l = n <- 100), colorRampPalette(hm_pal)(n))
-    ncell <- rep(NA,times=length(unique(cluster_numbers)))
-    names(ncell) <- unique(cluster_numbers)[order(unique(cluster_numbers))]
-    for(i in 1:length(ncell)) {
-      ncell[i] <- sum(cluster_numbers==names(ncell)[i])
-    }
-    pop.freq <- matrix(data=ncell,ncol=1)
-    row.names(pop.freq) <- names(ncell)
-    size_anno_nums <- round((pop.freq/sum(pop.freq))*100,2)
-    ranno1 <- rowAnnotation(`Cluster\nSize`=anno_barplot(pop.freq,border=F,width=unit(1.75, "cm"),
-                                                         axis_param=list(gp=gpar(fontsize=9)), axis = TRUE),
-                            annotation_name_gp=gpar(fontsize=10,fontface="bold"), name = "Cluster\nSize")
-    ranno2 <- rowAnnotation(frequency=anno_text(paste0(size_anno_nums,"%"),
-                                                gp=gpar(fontsize=10,fontface="bold")))
-    backend.matrix <- backend.matrix[order(row.names(backend.matrix)),]
     heatmap_output <- Heatmap(backend.matrix,col=color.map.fun,
                               row_names_side="left",
                               name="median\nscaled\nexpression",
