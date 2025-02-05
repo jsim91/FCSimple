18a7a39a4a4562d58522d4b62d2081d98cdf4b85
jsim91 <60735978+jsim91@users.noreply.github.com>
2025-02-05 15:35:22 -0600

    Update fcs_update.R

--- R/fcs_update.R
+++ R/fcs_update.R
@@ -1,9 +1,15 @@
-fcs_update <- function(fcs_join_obj)
+fcs_update <- function(fcs_join_obj, collection_instrument = instrument_type)
 {
-  if('object_history' %in% names(fcs_join_obj)) { # check if up to date first
+  if(all('object_history' %in% names(fcs_join_obj), "collection_instrument" %in% names(fcs_join_obj))) { # check if up to date first
     stop("object is already up to date")
-  } else {
-    fcs_join_obj[['object_history']] <- paste0("updated: ",Sys.time())
     return(fcs_join_obj)
+  } else {
+    if(any(length(collection_instrument)!=1, !collection_instrument %in% c("cytof","flow"))) {
+      stop("'collection_instrument should be one of: 'cytof' or 'flow'")
+    } else {
+      fcs_join_obj[['collection_instrument']] <- collection_instrument
+      fcs_join_obj[['object_history']] <- paste0("updated: ",Sys.time())
+      return(fcs_join_obj)
+    }
   }
 }
