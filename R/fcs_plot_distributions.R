907ed122c2e85f73997dad51f406d63a1c9f8dc8
jsim91 <60735978+jsim91@users.noreply.github.com>
2025-02-21 14:19:02 -0600

    Update fcs_plot_distributions.R

--- R/fcs_plot_distributions.R
+++ R/fcs_plot_distributions.R
@@ -1,6 +1,6 @@
 fcs_plot_distribution <- function(fcs_join_obj,
                                   override_correction = TRUE, 
-                                  separate_by = c("none", "date", "cluster"),
+                                  separate_by = c("none", "date", "cluster", "condition"),
                                   plot_algorithm = c("leiden","flowsom","louvain","phenograph","git"),
                                   outdir = getwd(),
                                   plot_palette = NULL,
@@ -41,6 +41,17 @@ fcs_plot_distribution <- function(fcs_join_obj,
                                     batch = obj_data[,"date"])
       colnames(data_split[[i]])[1] <- colnames(obj_data)[i]
     }
+    plot_set <- lapply(X = data_split, FUN = plot_date, rm0 = rm_zero, trq = trim_quantile, instr_type = fcs_join_obj[['collection_instrument']])
+    if(add_timestamp) {
+      fname <- paste0(outdir,"/panel_distributions_by_batch_",strftime(Sys.time(),"%Y-%m-%d_%H%M%S"),".pdf")
+    } else {
+      fname <- paste0(outdir,"/panel_distributions_by_batch.pdf")
+    }
+    ggsave(filename = fname,
+           plot = ggpubr::ggarrange(plotlist = plot_set, nrow = ceiling(sqrt(length(plot_set))),
+                                    ncol = ceiling(sqrt(length(plot_set))), legend = "bottom", common.legend = TRUE),
+           device = "pdf", width = ceiling(sqrt(length(plot_set)))*2.5, height = ceiling(sqrt(length(plot_set)))*2.5,
+           units = "in", dpi = 900, limitsize = FALSE)
   } else if(tolower(separate_by) == "cluster") {
     data_split <- vector("list", length = ncol(obj_data))
     names(data_split) <- colnames(obj_data)
@@ -54,8 +65,7 @@ fcs_plot_distribution <- function(fcs_join_obj,
     for(i in 1:length(data_split)) {
       data_split[[i]]$cluster <- factor(fcs_join_obj[[tolower(plot_algorithm)]]$clusters)
     }
-  }
-  if(tolower(separate_by) == "none") {
+  } else if(tolower(separate_by) == "none") {
     data_split <- vector("list", length = ncol(obj_data))
     names(data_split) <- colnames(obj_data)
     for(i in 1:length(data_split)) {
@@ -73,21 +83,6 @@ fcs_plot_distribution <- function(fcs_join_obj,
                                     ncol = ceiling(sqrt(length(plot_set)))), device = "pdf",
            width = ceiling(sqrt(length(plot_set)))*2.5, height = ceiling(sqrt(length(plot_set)))*2.5, units = "in",
            dpi = 900, limitsize = FALSE)
-  } else if(tolower(separate_by) == "date") {
-    if(!"batch" %in% colnames(data_split[[1]])) {
-      stop("error in argument 'separate_by': no run date found, cannot plot by run date/batch")
-    }
-    plot_set <- lapply(X = data_split, FUN = plot_date, rm0 = rm_zero, trq = trim_quantile, instr_type = fcs_join_obj[['collection_instrument']])
-    if(add_timestamp) {
-      fname <- paste0(outdir,"/panel_distributions_by_batch_",strftime(Sys.time(),"%Y-%m-%d_%H%M%S"),".pdf")
-    } else {
-      fname <- paste0(outdir,"/panel_distributions_by_batch.pdf")
-    }
-    ggsave(filename = fname,
-           plot = ggpubr::ggarrange(plotlist = plot_set, nrow = ceiling(sqrt(length(plot_set))),
-                                    ncol = ceiling(sqrt(length(plot_set))), legend = "bottom", common.legend = TRUE),
-           device = "pdf", width = ceiling(sqrt(length(plot_set)))*2.5, height = ceiling(sqrt(length(plot_set)))*2.5,
-           units = "in", dpi = 900, limitsize = FALSE)
   } else if(tolower(separate_by) == "cluster") {
     plot_set <- lapply(X = data_split, FUN = plot_cluster, rm0 = rm_zero, trq = trim_quantile, instr_type = fcs_join_obj[['collection_instrument']])
     if(add_timestamp) {
@@ -100,7 +95,31 @@ fcs_plot_distribution <- function(fcs_join_obj,
                                     ncol = ceiling(sqrt(length(plot_set))), legend = "bottom", common.legend = TRUE),
            device = "pdf", width = ceiling(sqrt(length(plot_set)))*3, height = ceiling(sqrt(length(plot_set)))*9,
            units = "in", dpi = 900, limitsize = FALSE)
-  }
+  } else if(tolower(separate_by) == "condition") {
+    if(!"condition" %in% names(fcs_join_obj)){
+      print("Unable to find condition. Using separate_by = 'none' instead.")
+    } else {
+      batch <- fcs_join_obj[["condition"]]
+      obj_data$condition <- batch
+    }
+    data_split <- vector("list", length = ncol(obj_data)-1)
+    names(data_split) <- colnames(obj_data)[1:(ncol(obj_data)-1)]
+    for(i in 1:length(data_split)) {
+      data_split[[i]] <- data.frame(val1 = obj_data[,which(colnames(obj_data)==names(data_split)[i])],
+                                    batch = obj_data[,"condition"])
+      colnames(data_split[[i]])[1] <- colnames(obj_data)[i]
+    }
+    plot_set <- lapply(X = data_split, FUN = plot_condition, rm0 = rm_zero, trq = trim_quantile, instr_type = fcs_join_obj[['collection_instrument']])
+    if(add_timestamp) {
+      fname <- paste0(outdir,"/panel_distributions_by_condition_",strftime(Sys.time(),"%Y-%m-%d_%H%M%S"),".pdf")
+    } else {
+      fname <- paste0(outdir,"/panel_distributions_by_condition.pdf")
+    }
+    ggsave(filename = fname,
+           plot = ggpubr::ggarrange(plotlist = plot_set, nrow = ceiling(sqrt(length(plot_set))),
+                                    ncol = ceiling(sqrt(length(plot_set))), legend = "bottom", common.legend = TRUE),
+           device = "pdf", width = ceiling(sqrt(length(plot_set)))*2.5, height = ceiling(sqrt(length(plot_set)))*2.5,
+           units = "in", dpi = 900, limitsize = FALSE)
 }
 
 plot_none <- function(input_data, rm0, trq, instr_type)
@@ -250,6 +269,57 @@ plot_cluster <- function(input_data, rm0, trq, instr_type)
     ggtitle(capture_channel) +
     theme(axis.text.y = element_text(),
           axis.title.x = element_blank(),
+          plot.title = element_text(hjust = 0.5))
+  return(plt1)
+}
+
+plot_condition <- function(input_data, rm0, trq, instr_type)
+{
+  capture_channel <- colnames(input_data)[1]
+  colnames(input_data)[1] <- "tmp"
+  if(instr_type=="flow") {
+    if(rm0) {
+      warning("remove 0s set to TRUE while instrument_type is 'flow'; leaving 0 values.")
+    }
+    if(!is.null(trq)) {
+      if(length(trq)!=2) {
+          stop("'trim_quantile' should be a vector of length 2")
+        }
+      trim_q <- as.numeric(quantile(x = input_data[,1], probs = trq))
+      drop_index <- union(which(input_data[,1]>max(trim_q)),which(input_data[,1]<min(trim_q)))
+      input_data <- input_data[-drop_index,]
+    }
+  }
+  if(instr_type=="cytof") {
+    if(rm0) {
+      rm0_ind <- which(input_data[,1]==0)
+      if(length(rm0_ind)!=0) {
+        input_data <- input_data[-which(input_data[,1]==0),]
+      }
+      if(!is.null(trq)) {
+        if(length(trq)!=1) {
+          stop("'trim_quantile' should be a singlet numeric value")
+        } 
+        trim_q <- as.numeric(quantile(x = input_data[,1], probs = trq))
+        input_data <- input_data[-which(input_data[,1]>trim_q),]
+      }
+    } else {
+      if(!is.null(trq)) {
+        if(length(trq)!=1) {
+          stop("'trim_quantile' should be a singlet numeric value")
+        } 
+        trim_q <- as.numeric(quantile(x = input_data[,1], probs = trq))
+        input_data <- input_data[-which(input_data[,1]>trim_q),]
+      }
+    }
+  }
+  plt1 <- ggplot(data = input_data, mapping = aes(x = tmp, group = condition,
+                                                  color = condition, fill = condition)) +
+    geom_density(size = 0.75, alpha = 0.1) +
+    theme_minimal() +
+    ggtitle(capture_channel) +
+    theme(axis.text.y = element_blank(),
+          axis.title = element_blank(),
           plot.title = element_text(hjust = 0.5))
   return(plt1)
 }
