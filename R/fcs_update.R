7e3b0b8fc3602a5d720311a4f4d6417b671d5ae4
jsim91 <60735978+jsim91@users.noreply.github.com>
2025-09-11 19:43:15 -0500

    Update fcs_update.R

--- R/fcs_update.R
+++ R/fcs_update.R
@@ -45,14 +45,25 @@
 #' @export
 fcs_update <- function(fcs_join_obj, instrument_type = c("cytof","flow"))
 {
-  if(all('object_history' %in% names(fcs_join_obj), "collection_instrument" %in% names(fcs_join_obj), 'metadata' %in%  names(fcs_join_obj))) { # check if up to date first
+  if(all('object_history' %in% names(fcs_join_obj), 
+         "collection_instrument" %in% names(fcs_join_obj), 
+         'run_date' %in%  names(fcs_join_obj), 
+         'metadata' %in% names(fcs_join_obj))) { # check if up to date first
     warning("object is already up to date")
     return(fcs_join_obj)
   } else {
     if(any(length(instrument_type)!=1, !instrument_type %in% c("cytof","flow"))) {
       stop("'collection_instrument should be one of: 'cytof' for mass cytometry or 'flow' for fluorescence cytometry")
     } else {
-      fcs_join_obj[['metadata']] <- data.frame(source = fcs_join_obj[['source']])
+      if(mean(c("data", "source", "run_date", "metadata") %in% names(fcs_join_obj))!=1) {
+        stop('fcs_join_obj must contain: "data", "source", "run_date", "metadata"')
+      }
+      src_data <- fcs_join_obj[['source']]
+      base_metadata <- data.frame(patient_ID = gsub(pattern = '[0-9]+\\-[A-Za-z]+\\-[0-9]+', 
+                                                    replacement = '', 
+                                                    x = src_data), 
+                                  run_date = fcs_join_obj[['run_date']])
+      fcs_join_obj[['metadata']] <- base_metadata[!duplicated(base_metadata$patient_ID),]
       fcs_join_obj[['collection_instrument']] <- instrument_type
       fcs_join_obj[['object_history']] <- paste0("updated: ",Sys.time())
       return(fcs_join_obj)
